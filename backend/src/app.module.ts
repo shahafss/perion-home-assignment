e3ab9f3a516967296496d665ed048900bfbe3c38
shahaf <shahaflidor@gmail.com>
2026-04-18 22:34:14 +0300

    remove irrelevant service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { DashboardModule } from './dashboard/dashboard.module';
 import { DatabaseModule } from './database/database.module';
 import { SeederModule } from './database/seeder.module';
 import { HealthController } from './health.controller';
@@ -13,7 +12,6 @@ import { UserModule } from './users/user.module';
     SeederModule,
     UserModule,
     AuthModule,
-    DashboardModule,
     RolesModule
   ],
   controllers: [HealthController]
